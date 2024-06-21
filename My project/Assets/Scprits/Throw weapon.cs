b87a99d8b20fd0ceebe8bd934514d3f7ed38b1eb
kimjinmo1 <jm020517@naver.com>
2024-06-21 19:59:04 +0900

    ㅠㅠ

--- My project/Assets/Scprits/Throw weapon.cs
+++ My project/Assets/Scprits/Throw weapon.cs
@@ -3,16 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Throwweapon : MonoBehaviour
-{
-    // Start is called before the first frame update
+{ 
+    Rigidbody2D rigid;
+    Vector2 force;
+    bool right;
+
+
+    private void Awake()
+    {
+       rigid = GetComponent<Rigidbody2D>(); 
+    }
+
     void Start()
     {
-        
+        rigid.AddForce(force, ForceMode2D .Impulse);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(new Vector3(0, 0, right == true ? -360f : 360f) * Time.deltaTime);
     }
 }
